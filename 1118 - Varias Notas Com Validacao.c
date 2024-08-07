dd43dea24283f272b53ad1426ef788218ea1bfce
Vinicius Castamann Giongo <159454133+Venilcito@users.noreply.github.com>
2024-08-07 14:07:22 -0300

    Add files via upload

---
+++ 1118 - Varias Notas Com Validacao.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 0;
+
+    do{
+        double ent1, ent2;
+        scanf("%lf", &ent1);
+
+        while(ent1 < 0 || ent1 > 10){
+            printf("nota invalida\n");
+            scanf("%lf", &ent1);
+        }
+        double nota1 = ent1;
+
+        scanf("%lf", &ent2);
+
+        while(ent2 < 0 || ent2 > 10){
+            printf("nota invalida\n");
+            scanf("%lf", &ent2);
+        }
+        double nota2 = ent2;
+
+        printf("media = %.2lf\n", (nota1 + nota2) / 2.0);
+
+        do{
+            printf("novo calculo (1-sim 2-nao)\n");
+            scanf("%d", &i);
+        } while(i > 2 || i < 1);
+        
+    } while(i == 1);
+
+    return 0;
+}
